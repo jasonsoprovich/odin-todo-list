756e2c26edd5cf8e63494e09515207e7fd4d2b8f
Jason Soprovich <190052169+jasonsoprovich@users.noreply.github.com>
2025-05-01 19:59:44 -0600

    feat: add css styling for todo-list and todo-form

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,33 +1,33 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, "../src/index.js"),
+  entry: path.resolve(__dirname, '../src/index.js'),
   output: {
-    path: path.resolve(__dirname, "../dist"),
-    filename: "bundle.[contenthash].js",
-    clean: true
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'bundle.[contenthash].js',
+    clean: true,
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ["babel-loader"]
+        use: ['babel-loader'],
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "../src/index.html"),
-      title: "Webpack Starter"
-    })
+      template: path.resolve(__dirname, '../src/index.html'),
+      title: 'Todo List',
+    }),
   ],
   resolve: {
-    extensions: [".js"]
-  }
+    extensions: ['.js'],
+  },
 };
