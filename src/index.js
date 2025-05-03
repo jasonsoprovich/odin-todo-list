320cfdf4be670d54cae12b6da2cc2df747b0520e
Jason Soprovich <190052169+jasonsoprovich@users.noreply.github.com>
2025-05-02 19:07:24 -0600

    feat: replace todo array with localStorage list

--- src/index.js
+++ src/index.js
@@ -1,28 +1,29 @@
 import './styles.css';
 import { qs, renderTodos } from './dom';
-import { createTodo } from './todo';
+import { loadTodos, getTodos, createTodo, deleteTodo } from './todo';
 
 const form = qs('#todo-form');
 const input = qs('#todo-input');
 const listElement = qs('#todo-list');
-const todos = [];
+
+loadTodos();
+renderTodos(getTodos(), listElement);
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const text = input.value.trim();
   if (!text) return;
-  todos.push(createTodo(text));
-  renderTodos(todos, listElement);
+  createTodo(text);
+  // todos.push(createTodo(text));
+  renderTodos(getTodos(), listElement);
   input.value = '';
   input.focus();
 });
 
 listElement.addEventListener('click', (e) => {
-  if (!e.target.matches('button.remove')) return;
   const id = Number(e.target.getAttribute('data-id'));
-  const index = todos.findIndex((todo) => todo.id === id);
-  if (index > -1) {
-    todos.splice(index, 1);
-    renderTodos(todos, listElement);
+  if (e.target.matches('button.remove')) {
+    deleteTodo(id);
   }
+  renderTodos(getTodos(), listElement);
 });
