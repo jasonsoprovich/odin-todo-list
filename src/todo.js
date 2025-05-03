5f41bede2b723038eb573abb4d61fb89eeae906a
Jason Soprovich <190052169+jasonsoprovich@users.noreply.github.com>
2025-05-02 18:57:04 -0600

    feat: add load/get/delete todo functions

--- src/todo.js
+++ src/todo.js
@@ -1,9 +1,24 @@
 const STORAGE_KEY = 'todos-app';
-const todos = [];
+let todos = [];
 let nextID = 1;
 
+export function loadTodos() {
+  const jsonItem = localStorage.getItem(STORAGE_KEY);
+  if (!jsonItem) return;
+  try {
+    todos = JSON.parse(jsonItem);
+    const maxID = todos.reduce((max, todo) => Math.max(max, todo.id), 0);
+    nextID = maxID + 1;
+  } catch {
+    todos = [];
+  }
+}
 export function saveTodos() {
   localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
+}
+
+export function getTodos() {
+  return todos;
 }
 
 export function createTodo(text) {
@@ -12,4 +27,12 @@ export function createTodo(text) {
   nextID += 1;
   saveTodos();
   return todo;
+}
+
+export function deleteTodo(id) {
+  const index = todos.findIndex((todo) => todo.id === id);
+  if (index === -1) return false;
+  todos.splice(index, 1);
+  saveTodos();
+  return true;
 }
