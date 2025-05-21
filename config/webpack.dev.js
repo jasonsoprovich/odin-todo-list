88ddba6b1aa3868cf5c77a809e204ea532c96bf5
Jason Soprovich <190052169+jasonsoprovich@users.noreply.github.com>
2025-05-20 23:27:10 -0600

    fix: webpack dev server and script

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,13 +1,20 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const path = require('path');
 
 module.exports = (env, argv) =>
   merge(common(env, argv), {
     mode: 'development',
     devtool: 'inline-source-map',
     devServer: {
-      static: './dist',
+      static: {
+        directory: path.resolve(__dirname, '../dist'),
+      },
       hot: true,
       open: true,
+      watchFiles: ['src/**/*'],
+      client: {
+        overlay: true, // show compile errors in browser
+      },
     },
   });
