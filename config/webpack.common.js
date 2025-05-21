8ee8d90fa96a8753bca0da56a46cdcbd6047156e
Jason Soprovich <190052169+jasonsoprovich@users.noreply.github.com>
2025-05-20 22:02:49 -0600

    fix: adjust webpack and package.json for css

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,42 +1,50 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = {
-  entry: path.resolve(__dirname, '../src/index.js'),
-  output: {
-    path: path.resolve(__dirname, '../dist'),
-    filename: 'bundle.[contenthash].js',
-    publicPath: './',
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
-      {
-        test: /\.css$/,
-        use: [
-          isProd ? MiniCssExtractPlugin.loader : 'style-loader',
-          'css-loader',
-        ],
-      },
+module.exports = (env, argv) => {
+  const isProd = process.env.NODE_ENV === 'production';
+
+  return {
+    entry: path.resolve(__dirname, '../src/index.js'),
+    output: {
+      path: path.resolve(__dirname, '../dist'),
+      filename: '[name].[contenthash].js',
+      publicPath: './',
+      clean: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: ['babel-loader'],
+        },
+        {
+          test: /\.css$/,
+          use: [
+            isProd ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader',
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, '../src/index.html'),
+        inject: 'body',
+        title: 'Todo List',
+      }),
+      ...(isProd
+        ? [
+            new MiniCssExtractPlugin({
+              filename: '[name].[contenthash].css',
+            }),
+          ]
+        : []),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '../src/index.html'),
-      title: 'Todo List',
-    }),
-    new MiniCssExtractPlugin({
-      filename: isProd ? '[name].[contenthash].css' : '[name].css',
-    }),
-  ],
-  resolve: {
-    extensions: ['.js'],
-  },
+    resolve: {
+      extensions: ['.js'],
+    },
+  };
 };
